eb04d76b673e8f9e127f249c5b3468b7ee07291e
danlof <danmusyoki.dk@gmail.com>
2024-07-03 21:10:26 +0300

    AIC

--- Drugs.py
+++ Drugs.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 
+from statsmodels.tsa.seasonal import seasonal_decompose,STL
+from statsmodels.tsa.stattools import adfuller
+from itertools import product
+
 # import data 
 df = pd.read_csv("/media/danlof/dan_files/data_science_codes/Timeseries/Antidiabetic/AusAntidiabeticDrug.csv")
 df.head()
@@ -16,4 +20,96 @@ fig, ax = plt.subplots()
 ax.plot(df.y)
 ax.set_xlabel('Date')
 ax.set_ylabel('Number of drug prescriptions')
-plt.xticks(np.)
+plt.xticks(np.arange(6,203,12),np.arange(1992,2009,1))
+fig.autofmt_xdate()
+plt.tight_layout()
+
+# Step b:Exploration
+decomposition = STL(df.y,period=12).fit()
+fig,(ax1,ax2,ax3,ax4) = plt.subplots(nrows=4,ncols=1,sharex=True,figsize=(10,8))
+ax1.plot(decomposition.observed)
+ax1.set_ylabel("Observed")
+
+ax2.plot(decomposition.trend)
+ax2.set_ylabel("Trend")
+
+ax3.plot(decomposition.seasonal)
+ax3.set_ylabel("Seasonal")
+
+ax4.plot(decomposition.resid)
+ax4.set_ylabel("Residuals")
+
+plt.xticks(np.arange(6,203,12),np.arange(1992,2009,1))
+fig.autofmt_xdate()
+plt.tight_layout()
+
+# step c: 
+## the SARIMA() model is picked because there is seasonality 
+## we dont pick the VAR because we not dealing with multiple interrelated timeseries data 
+
+# Step d .1 ,Check for stationarity 
+ad_fuller_result = adfuller(df.y)
+print(f"ADF statistics : {ad_fuller_result[0]}")
+print(f"p-value: {ad_fuller_result[1]}")
+
+# difference 1st order 
+y_diff = np.diff(df.y,n=1)
+ad_fuller_result=adfuller(y_diff)
+print(f'ADF Statistic: {ad_fuller_result[0]}')
+print(f'p-value: {ad_fuller_result[1]}')
+
+# differencing 1st order with n=12
+y_diff_seasonal = np.diff(df.y,n=12)
+ad_fuller_result=adfuller(y_diff_seasonal)
+print(f'ADF Statistic: {ad_fuller_result[0]}')
+print(f'p-value: {ad_fuller_result[1]}') 
+
+#Therefore d = 1,D=1,m=12 -> SARIMA(p,1,q)(P,1,Q)_12 
+
+# model selection
+# we chose the parameters that minimize the Akaike information criterion
+
+train = df.y[:168]
+test= df.y[168:]
+
+# function to find values of the parameters 
+from typing import Union
+from tqdm import tqdm
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+
+def optimize_SARIMAX(endog: Union[pd.Series,list],exog: Union[pd.Series,list],order_list,d:int,D:int,s:int) -> pd.DataFrame:
+    results = []
+    for order in tqdm(order_list):
+        try:
+            model = SARIMAX(
+                endog,
+                exog,
+                order = (order[0],d,order[1]),
+                seasonal_order=(order[2],D,order[3],s),
+                simple_differencing=False
+            ).fit(disp=False)
+        except:
+            continue
+
+        aic= model.aic
+        results.append([order,model.aic])
+    
+    result_df = pd.DataFrame(results)
+    result_df.columnns = ['(p,q,P,Q)','AIC']
+
+    result_df = result_df.sort_values(by='AIC', ascending=True).reset_index(drop=True)
+    return result_df
+
+
+ps = range(0, 5, 1)
+qs = range(0, 5, 1)
+Ps = range(0, 5, 1)
+Qs = range(0, 5, 1)
+
+order_list = list(product(ps, qs, Ps, Qs))
+d = 1
+D = 1
+s = 12
+
+SARIMA_result_df = optimize_SARIMAX(train, None, order_list, d, D, s)
+print(SARIMA_result_df)
--- README.md
+++ README.md
@@ -6,4 +6,15 @@
 - The objective of the project is forecasting the number of antidiabetic drug prescriptions in Australia, from 1991 to 2008.
 - To solve the problem of overproduction of the antidiabetic drugs by predictioning how much is needed.
 
-#### Results
+#### Steps
+- a. Visualize the timeseries
+- b. Use timeseries decomposition to extract trend and seasonality.
+- c. Determine the most suitable model
+- d. model the series with:
+    - apply transformation to make it stationary
+    - set the values of *d* and D. set the value of *m*
+    - find the optimal (p,d,q)(P,D,Q)*<sub>m</sub>*
+    - perform residual analysis to validate the model.
+- e. Perform rolling forecast of 12 months on the test set.
+- f. Visualize your forecasts.
+- g. Compare the model's performance to a baseline 
